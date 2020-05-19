36c420927cb3cdd77c075487fbdd1b552ec3cb7c
OleksandrCh <oleksandrchernii.0@gmail.com>
2020-05-19 21:33:27 +0300

    проверка c async

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const transferItemDir = require('./homework_1/hw1')
+const transferItemDir = require('./homework_1/hw1');
 
 const directory2000 = '2000';
 const directory1800 = '1800';
--- homework_1/hw1.js
+++ homework_1/hw1.js
@@ -3,42 +3,60 @@ const path = require('path');
 
 const transferItemDir = (directory2000, directory1800) => {
     const inconstantDir = 'inconstantDir';
-    makeDir(inconstantDir);
+    const arr = [[directory2000, inconstantDir], [directory1800, directory2000], [inconstantDir, directory1800]];
 
-    removeFile(directory2000, inconstantDir);
-    removeFile(directory1800, directory2000);
-    removeFile(inconstantDir, directory1800);
+    // arr.forEach(itemArr => {
+    //         const [from, to] = itemArr;
+    //     console.log('----------');
+    //     console.log(from, to);
+    //     console.log('----------');
+    //        removeFile(from, to);
+    // });
+    makeDir(inconstantDir)
+        .then(()=>{
+            moveFile(directory2000, inconstantDir)
+        })
+        .then(() => {
+            moveFile(directory1800, directory2000)
+        })
+        .then(() => {
+            moveFile(inconstantDir, directory1800)
+        })
+        .then(()=>{
+            removeDir(inconstantDir)
+        })
 
-    removeDir(inconstantDir)
+
+
 };
 
 const removeDir = (name) => {
     fs.rmdir(path.join(__dirname, name), err => {
-        console.log(err)
+        if (err) console.log('removeDir ',err)
     })
 };
 
-const makeDir = (name) => {
+const makeDir = async (name) => {
     fs.mkdir(
         path.join(__dirname, name),
         err => {
-            console.log(err)
+            if (err) console.log('makeDir ',err)
         });
 };
 
-const removeFile = (from, to) => {
+const moveFile = (from, to) => {
     fs.readdir(path.join(__dirname, from),
         (err, files) => {
             if (err) {
-                console.log(err);
-                return
+                if (err) console.log('readdir ',err)
             }
             files.forEach(student => {
+                console.log(student)
                 fs.rename(
                     path.join(__dirname, from, student),
                     path.join(__dirname, to, student),
                     err => {
-                        console.log(err)
+                        if (err) console.log('removeFile ',err)
                     })
             })
         });
