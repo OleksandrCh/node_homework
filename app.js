0d11c6f044b6bb2cce501e07c15ba6b8828db1ad
OleksandrCh <oleksandrchernii.0@gmail.com>
2020-05-19 20:09:30 +0300

    проверка

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const transferItemDir = require('./homework_1/hw1')
+
 const directory2000 = '2000';
 const directory1800 = '1800';
 
--- homework_1/hw1.js
+++ homework_1/hw1.js
@@ -2,51 +2,46 @@ const fs = require('fs');
 const path = require('path');
 
 const transferItemDir = (directory2000, directory1800) => {
+    const inconstantDir = 'inconstantDir';
+    makeDir(inconstantDir);
+
+    removeFile(directory2000, inconstantDir);
+    removeFile(directory1800, directory2000);
+    removeFile(inconstantDir, directory1800);
+
+    removeDir(inconstantDir)
+};
+
+const removeDir = (name) => {
+    fs.rmdir(path.join(__dirname, name), err => {
+        console.log(err)
+    })
+};
+
+const makeDir = (name) => {
     fs.mkdir(
-        path.join(__dirname, 'inconstantDir'),
+        path.join(__dirname, name),
         err => {
             console.log(err)
         });
+};
 
-    fs.readdir(path.join(__dirname, directory2000),
+const removeFile = (from, to) => {
+    fs.readdir(path.join(__dirname, from),
         (err, files) => {
+            if (err) {
+                console.log(err);
+                return
+            }
             files.forEach(student => {
                 fs.rename(
-                    path.join(__dirname,directory2000 , student),
-                    path.join(__dirname, 'inconstantDir', student),
+                    path.join(__dirname, from, student),
+                    path.join(__dirname, to, student),
                     err => {
                         console.log(err)
                     })
             })
         });
-
-    fs.readdir(path.join(__dirname, directory1800),
-        (err, files) => {
-            files.forEach(student => {
-                fs.rename(
-                    path.join(__dirname, directory1800, student),
-                    path.join(__dirname, directory2000, student),
-                    err => {
-                        console.log(err)
-                    })
-            })
-        });
-
-    fs.readdir(path.join(__dirname, 'inconstantDir'),
-        (err, files) => {
-            files.forEach(student => {
-                fs.rename(
-                    path.join(__dirname, 'inconstantDir', student),
-                    path.join(__dirname, directory1800, student),
-                    err => {
-                        console.log(err)
-                    });
-            });
-        });
-
-    fs.rmdir(path.join(__dirname, 'inconstantDir'), err => {
-        console.log(err)
-    })
 };
 
 module.exports = transferItemDir;
